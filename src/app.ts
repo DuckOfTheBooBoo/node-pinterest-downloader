8ebffea5735f3e43760ee0639b832ea6eb41363c
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-06-19 20:45:09 +0700

    Initial configuration

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello');
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+const PORT: number = 8080;
+
+app.get('/', (req: Request, res: Response,  next: NextFunction) => {
+    res.status(200).send({
+        data: 'Hello from Arajdian Altaf'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at port ${PORT}`);
+});
