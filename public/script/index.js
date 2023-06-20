7ef6ff2a092db6ee82c8cff3128701461c778e70
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-06-20 21:33:08 +0700

    Added download button handling and styles

--- public/script/index.js
+++ public/script/index.js
@@ -1,6 +1,10 @@
 $(function () {
   $('#form-pin-download').on('submit', function (event) {
     event.preventDefault()
+
+    if ($('.download-container').css('display') !== 'none') {
+      $('.download-container').slideToggle()
+    }
     const formData = new FormData(event.target)
     const data = {}
 
@@ -10,11 +14,16 @@ $(function () {
 
     $('#btn-icon').addClass('fa-spinner fa-spin').removeClass('fa-download')
 
+    const downloadLink = $('#download-link')
+
     $.ajax({
       url: `/download?url=${data['pin-board-url']}`,
       type: 'POST',
       success: function (response) {
         $('#btn-icon').addClass('fa-download').removeClass('fa-spinner fa-spin')
+        $('.download-container').slideToggle('slow')
+        downloadLink.text(response.filename)
+        downloadLink.attr('href', response.downloadUrl)
       },
       error: function (xhr, status, error) {
         console.error(error)
